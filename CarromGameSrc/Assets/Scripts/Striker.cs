3d730437621d598d3461930c09fcc04d359b8045
RUPAM_PC\rupam <rdebmail@gmail.com>
2022-10-23 16:32:05 +0530

    Added windows and Android different scenes for respective build

--- CarromGameSrc/Assets/Scripts/Striker.cs
+++ CarromGameSrc/Assets/Scripts/Striker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Striker : MonoBehaviour
@@ -19,6 +20,13 @@ public class Striker : MonoBehaviour
     float powerApplied = 10f;
 
     public Image powerStats;
+    public Slider posSlider;
+    //[SerializeField] 
+    //EventSystem m_EventSystem;
+    //[SerializeField] 
+    //GraphicRaycaster m_Raycaster;
+    public GameObject Powerbar;
+    bool powerFixed = false;
     void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
@@ -26,36 +34,24 @@ public class Striker : MonoBehaviour
         selfTransform = transform;
         arrowTransform = arrowDirection.transform;
     }
-
     void Update()
     {
         
         worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //arrowTransform.LookAt(Input.mousePosition, arrowTransform.up);
-        arrowTransform.LookAt(Input.mousePosition, Vector2.up);
         direction = (Vector2)((worldMousePos - transform.position));
         direction.Normalize();
-        //arrowTransform.LookAt(worldMousePos);
+        //Debug.Log(positionIsSet);
         if (!hasStriked && !positionIsSet)
         {
-            selfTransform.position = new Vector2(Mathf.Clamp(worldMousePos.x, -2.71f, 2.65f), startPosition.y);
+            //selfTransform.position = new Vector2(Mathf.Clamp(worldMousePos.x, -2.71f, 2.65f), startPosition.y);
+            selfTransform.position = new Vector2(Mathf.Clamp(posSlider.value, -2.71f, 2.65f), startPosition.y);
             rbody.angularVelocity =0f;
-            //if (Input.GetMouseButtonDown(0))
-            //    selfTransform.position = Input.mousePosition;
         }
 
-        if (positionIsSet && rbody.velocity.magnitude == 0)
-        {
-            arrowTransform.gameObject.SetActive(true);
-        }
 
         if (Input.GetMouseButtonDown(1))
         {
-            if (!positionIsSet)
-            {
-                positionIsSet = true;
-            }
-
+            PlaceButton();
         }
         if(positionIsSet)
         {
@@ -64,13 +60,23 @@ public class Striker : MonoBehaviour
                 powerApplied += 0.1f;
                 powerStats.fillAmount = (float)powerApplied / 100;
                 strikerSpeed = (int)(powerApplied * 50f);
+                if (powerStats.fillAmount == 1)
+                    powerApplied = 0f;
             }
         }
-        if (Input.GetMouseButtonUp(0) && rbody.velocity.magnitude < 1 && !hasStriked && positionIsSet)
-        {                
-            ShootStriker();
-        }
+#if UNITY_ANDROID
+        DragShoot();
+        GeneratePower();
 
+#endif
+
+#if UNITY_EDITOR
+        if (Input.GetButtonUp("Fire1") && rbody.velocity.magnitude < 1 && !hasStriked && positionIsSet && !EventSystem.current.IsPointerOverGameObject()) //  
+        {
+            //if (Input.GetMouseButtonUp(0))
+                ShootStriker();
+        }
+#endif
         if (rbody.velocity.magnitude < 0.2f && rbody.velocity.magnitude != 0)
         {
             StrikerReset();
@@ -80,10 +86,12 @@ public class Striker : MonoBehaviour
     public void StrikerReset()
     {
         rbody.velocity = Vector2.zero;
+        rbody.angularVelocity = 0;
         hasStriked = false;
         positionIsSet = false;
         powerStats.fillAmount = 0;
         powerApplied = 0f;
+        powerFixed = false;
     }
 
     public void ShootStriker()
@@ -94,5 +102,73 @@ public class Striker : MonoBehaviour
         rbody.AddForce(direction * strikerSpeed);
         hasStriked = true;
     }
+#if UNITY_EDITOR
+    public void PlaceButton()
+    {
+        if (!positionIsSet)
+        {
+            positionIsSet = true;
+        }
+        if (EventSystem.current.currentSelectedGameObject.GetComponent<Button>() != null)
+            Debug.Log("Hallelu");
+    }
+#endif
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StrikerReset();
+        selfTransform.position = new Vector2(Random.Range(-2.71f, 2.65f), startPosition.y);
+        positionIsSet = true;
+    }
 
+    void DragShoot()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 startPos = Camera.main.ScreenToWorldPoint(touch.position);
+            //Debug.Log(startPos);
+            RaycastHit2D hit = Physics2D.Raycast(startPos, -Vector2.up);
+            if (hit && hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("I m hit");
+            }
+        }
+    }
+
+    void GeneratePower()
+    {
+        if(!powerFixed)
+        {
+            powerApplied += 0.1f;
+            powerStats.fillAmount = (float)powerApplied / 100;
+            strikerSpeed = (int)(powerApplied * 50f);
+            if (powerStats.fillAmount == 1)
+                powerApplied = 0f;
+        }
+
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            PointerEventData pointer = new PointerEventData(EventSystem.current);
+            pointer.position = Input.mousePosition;
+
+            List<RaycastResult> raycastResults = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointer, raycastResults);
+
+            if (raycastResults.Count > 0)
+            {
+                foreach (var go in raycastResults)
+                {                   
+                    if (go.gameObject == Powerbar)
+                    {
+                        //Debug.Log(go.gameObject.name);
+                        Debug.Log("power hit");
+                        //powerStats.fillAmount = (float)powerApplied / 100;
+                        powerFixed = true;
+                    }
+                }
+
+            }
+        }
+
+    }
 }
